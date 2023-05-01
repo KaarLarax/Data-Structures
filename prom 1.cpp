d113e7016b258819e4f1a96a430b4bfeb0d68b0d
Kaarl Lara <114048858+DARKOLARASLOWED@users.noreply.github.com>
2023-04-30 20:15:28 -0600

    Programa 1
    Escribe la definición de una función cuya tarea es encontrar el valor menor de un arreglo de enteros. La función deberá ser de tipo int con el nombre de menor y deberá de recibir 2 parámetros, uno de tipo arreglo y otro de tipo int para el tamaño del arreglo. En la función principal main(), declara un arreglo de 6 números enteros y solicita sus valores por teclado. Finalmente, manda llamar (invocar) la función e imprime el valor menor del arreglo (devuelto por la función). No olvides incluir el prototipo de la función.

---
+++ prom 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+int minimo (int [], int );
+void leer_array (int [], int );
+
+int main()
+{
+    int *puntero, n;
+    cout << "¿De cuantos valores será su arreglo? " << endl;
+    cin >> n;
+    puntero = new int [n];
+    cout << "Ingrese los valores" << endl;
+    leer_array(puntero, n);
+    cout << "El valor minimo de su arreglo es: " << minimo(puntero, n) << endl;
+
+    delete puntero;
+    return 0;
+}
+ int minimo (int x[], int y)
+ {
+     int menor=x[0];
+     for (int i = 1; i < y; i++){
+            if (menor > x[i]){
+                menor = x[i];
+            }
+     }
+     return menor;
+ }
+void leer_array (int x[], int y)
+{
+    for (int i = 0; i < y; i++){
+        cout << endl << "Ingresa un numero " << i + 1 << ": ";
+        cin >> x[i];
+    }
+}
